e4edb8e8a96be84dd00c867513491dec3ba8e4a0
Esp <rlw1972.ru1984@gmail.com>
2025-09-03 20:55:48 +0500

    Integer variable

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
 
-    public double calculate(double height, int weight) {
-        double bmi = (int) (weight / Math.pow(height, 2));
-        return bmi;
+    public int calculate(double height, int weight) {
+        double bmi = (weight / Math.pow(height, 2));
+        return (int) bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         double height = 1.87; //Рост
         int weight = 98; //Вес
 
-        double index = (int) service.calculate(height, weight);
+        double index = service.calculate(height, weight);
 
         System.out.println("Индекс массы тела = " + Math.round(index));
     }
